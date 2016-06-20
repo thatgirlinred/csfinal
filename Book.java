8480d9d05ba98945f335849f2719870f09a70675
thatgirlinred <aquamarine2426@gmail.com>
2016-06-20 19:13:33 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,23 +1,99 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.applet.*;
+import java.util.ArrayList;
+import javax.swing.*;
 
-public class Book extends JFrame implements ActionListener, ItemListener
+public class Book extends JApplet implements ActionListener
 {
-	JButton next, choiceOne, choiceTwo, home;
+	JLabel display = new JLabel();
+	
+	Button choiceOne = new Button("Choice 1");
+	Button choiceTwo = new Button("Choice 2");
+	Button home = new Button("Home");
+	Button next = new Button("Next Page");
 
-	public Book()
+	int r = 0;
+	int c = 0;
+	
+	String background[][] = { {"temp00.jpg"},
+				{"temp10.jpg", "temp12.jpg"), 
+				{"temp20.jpg", "temp21.jpg", "temp22.jpg", "temp23.jpg"}, 
+				{"temp30.jpg", "temp31.jpg", "temp32.jpg", "temp33.jpg"}};
+	
+
+	public void init()
 	{
-		getContentPane().setLayout();
-		//Panel for book page image
+		JPanel buttons = new JPanel(new FlowLayout());
 
-		//Panel for choices
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add("Center", display);
+		getContentPane().add("South", buttons);
+
+		buttons.add(home);
+		home.addActionListener(this);
+
+		if (background[].length == 4)
+		{
+			buttons.add(next);
+			next.addActionListener(this);
+		}
+		
+		if (background[].length != 4)
+		{
+			buttons.add(choiceOne);
+			choiceOne.addActionListener(this);
+			
+			buttons.add(choiceTwo);
+			choiceTwo.addActionListener(this);
+		}
 	}
-	//Method to choose option 1
+	public void showCurrentPage()
+	{
+		display.setIcon(new ImageIcon(background[r][c]));
+	}
 
+	public void showNextPage()
+	{
+		if (r+1 <= background.length)
+			display.setIcon(new ImageIcon(background[r++]));
+		else
+		{
+			r = 0;
+			c = 0;
+		}
+	}
+
+	public void showBackPage()
+	{
+		if (r-1 > -1)
+			display.setIcon(new ImageIcon(background[r--]));
+		else
+			r = 0;
+	}
+
+	public void showHomePage()
+	{
+		display.setIcon(new ImageIcon(background[0]));
+	}
+
+	//Method to choose option 1
+	{
+		
+	}
 	//Method to choose option 2
+	
 	public void actionPerformed(ActionEvent evt)
 	{
 		if (evt.getSource() == choiceOne)
+			showChoiceOne();
+		if (evt.getSource() == choiceTwo)
+			showChoiceTwo();	
+		if (evt.getSource() == next)
+			showNextPage();
+		if (evt.getSource() == back)
+			showBackPage();
+		if (evt.getSource() == home)
+			showHomePage();
 	}
 }
