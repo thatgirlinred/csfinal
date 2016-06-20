459ddd1305881c766d4ff1df054ddb3b53443985
thatgirlinred <aquamarine2426@gmail.com>
2016-06-20 21:53:51 +0000

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Book extends JFrame implements ActionListener, ItemListener
+{
+	JButton next, choiceOne, choiceTwo, home;
+
+	public Book()
+	{
+		getContentPane().setLayout();
+		//Panel for book page image
+
+		//Panel for choices
+	}
+	//Method to choose option 1
+
+	//Method to choose option 2
+	public void actionPerformed(ActionEvent evt)
+	{
+		if (evt.getSource() == choiceOne)
+	}
+}
---
+++ BookMain.java
@@ -0,0 +1,7 @@
+public class BookMain
+{
+	public static void main(String[] args)
+	{
+		Book stuff = new Book();
+	}
+}
